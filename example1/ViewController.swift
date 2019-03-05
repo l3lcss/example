95bd9d70a64439847bd540a79aaaef233315cb12
princel3lcS <princeboy__@hotmail.com>
2019-03-05 09:36:20 +0700

    Initial Commit

---
+++ example1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  example1
+//
+//  Created by Admin on 5/3/2562 BE.
+//  Copyright © 2562 th.ac.kmutnb.www. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
